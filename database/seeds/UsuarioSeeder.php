9797e07dd2c23ef833cd80bc77483f1b81f5facc
geralsantos <geral_366@hotmail.com>
2020-05-18 19:02:36 -0500

    d

--- database/seeds/UsuarioSeeder.php
+++ database/seeds/UsuarioSeeder.php
@@ -14,8 +14,17 @@ class UsuarioSeeder extends Seeder
         DB::table('usuario')->truncate();
         
         DB::table('usuario')->insert([
-            'perfil_id' => 1,
-            'usuario' => 'geral',
+            'perfil_id' => 2,
+            'usuario' => 'tecnico',
+            'password' => Hash::make('123'),
+            'usuario_creacion' => 1,
+            'usuario_edicion' => 1,
+            'fecha_creacion' => now()->format('Y-m-d H:i:s'),
+            'fecha_modificacion' => now()->format('Y-m-d H:i:s'),
+        ]);
+        DB::table('usuario')->insert([
+            'perfil_id' => 3,
+            'usuario' => 'cliente',
             'password' => Hash::make('123'),
             'usuario_creacion' => 1,
             'usuario_edicion' => 1,
